1cfeaa9cee520987a5a0a18c344d1aa66ca969fd
Jazgul Turgunova <jazgul.turgunova.alt@gmail.com>
2022-06-03 16:47:18 +0600

    update files

--- script.js
+++ script.js
@@ -1,103 +1,33 @@
-//1 - Программа которая считает количество секунд в часе.
-const countSeconds = (hour) => {
-  let hourToSeconds;
-  hourToSeconds = hour * 3600;
-  console.log(hour + " - " + hourToSeconds + " seconds");
-};
+//1. Напишите функцию, которая может преобразовывать строку в число - 10 баллов
 
-countSeconds(3);
+function strToNumber(str) {
+  str = Number(str);
+  return str;
+}
 
-// 2 - Переменная myNumber с значением 2022.тВывод значение этой переменной на экран с помощью метода alert.
+console.log(strToNumber("12"));
 
-const myNumber = 2022;
-alert(myNumber);
+//2. Напишите функцию, которая проверяет число четное или не четное: если четное возвращает **`true`** иначе **`false`** - 10 баллов
 
-// 3 - Спросите имя пользователя с помощью метода prompt. Вывод сообщение на страницу с помощью document.write : “Рад знакомству {имя пользователя}!”.
+function Number(a, b) {
+  if (a % b === 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
 
-const name = prompt("Введите имя: ");
-document.write("Рад знакомству " + name);
+Number(18, 2);
 
-// 4 - Даны два числа a, b. Написанная программа, которая считает среднее арифметическое двух чисел.
+//3. Сделайте функцию, которая определяет, есть в массиве элемент с заданным текстом или нет. Функция первым параметром должна принимать текст элемента, а вторым - массив, в котором делается поиск. Функция должна возвращать **`true`** или **`false` -** 10 баллов
 
-const avarageNumber = (firstNumber, secondNumber) => {
-  let a = firstNumber;
-  let b = secondNumber;
-  let sum;
-  sum = (a + b) / 2;
-  console.log("среднее арифметическое двух чисел: " + sum);
-};
+function ElementInArray(array, element) {
+  let result = array.includes(element);
+  return result;
+}
 
-avarageNumber(15, 45);
+let userNames = ["Rayana", "Shabdan", "Aisha", "Erhan"];
 
-// 5 - Спросите пользователя ввести любое число. Ваша задача вывести следующий по счету число.
-//   *Пример: Пользователь вводит число 10. Программа выводит число 11.*
-
-const nextNumber = (number) => {
-  let result = number + 1;
-  console.log(result);
-};
-
-nextNumber(10);
-
-// 6 - Создайте объект описывающий пользователя, в данном объекте должны быть следующие ключи:
-// - Имя
-// - Возраст
-// - Список друзей
-// - Почтовый адрес
-// - Электронный адрес
-
-const user = {
-  name: "Jazgul",
-  age: 24,
-  friends: ["Rayana", "Shabdan"],
-  postalCode: "-",
-  email: "jazgul.turgunova.alt@gmail.com",
-};
-
-// 7 - Создайте объект описывающий блог-пост на сайте:
-// - Название поста
-// - Автор поста
-// - Дата публикации поста
-// - Тело поста (содержит текст поста)
-// - Картинка поста (содержит url картинки)
-// - Теги поста
-
-const blogPost = {
-  name: "Like father - like sun.",
-  author: "Jazgul",
-  publishedAt: 2020,
-  postsBody:
-    "Once upon a time there lived a lion in a forest. One day after a heavy meal. It was sleeping under a tree. After a while, there came a mouse and it started to play on the lion. Suddenly the lion got up with anger and looked for those who disturbed its nice sleep. Then it saw a small mouse standing trembling with fear. The lion jumped on it and started to kill it. The mouse requested the lion to forgive it. The lion felt pity and left it. The mouse ran away.On another day, the lion was caught in a net by a hunter. The mouscommite came there and cut the net. Thus it escaped. There after, the mouse and the lion became friends. They lived happily in the forest afterwards.",
-  postsImg:
-    "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.picturequotes.com%2Fa-friend-in-need-is-a-friend-indeed-quote-19566&psig=AOvVaw04NWOKc7AcXrGnuM3MzxAb&ust=1653224649513000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLDRofzT8PcCFQAAAAAdAAAAABAR",
-  postsTag: '<img src="">',
-};
-
-// 8 - Массив с именами пользователей.
-
-let users = ["Rayan", "Kanat", "Sabyr", "Artur", "Melek"];
-
-// 9 - Массив с url адресами на разные картинки.
-
-let imgUrl = [
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fiso.500px.com%2F11-outstanding-photos-show-connections%2F&psig=AOvVaw3y4y9xOHsQkpwcrM0ZLZhl&ust=1653225062743000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIiu_7fV8PcCFQAAAAAdAAAAABAD",
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffr-fr.facebook.com%2FGrafixartPhotography%2Fposts&psig=AOvVaw3y4y9xOHsQkpwcrM0ZLZhl&ust=1653225062743000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIiu_7fV8PcCFQAAAAAdAAAAABAJ",
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pandasecurity.com%2Fen%2Fmediacenter%2Fsocial-media%2F5-tips-protect-photos-facebook%2F&psig=AOvVaw3y4y9xOHsQkpwcrM0ZLZhl&ust=1653225062743000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIiu_7fV8PcCFQAAAAAdAAAAABAP",
-];
-
-// 10 - Создайте массив содержащий объекты описывающие блог-посты (схема объектов идентична как в пункте 7.
-
-let blog_Post = [
-  { name: "Like father - like sun." },
-  { author: "Jazgul" },
-  { publishedAt: 2020 },
-  {
-    postsImg:
-      "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.picturequotes.com%2Fa-friend-in-need-is-a-friend-indeed-quote-19566&psig=AOvVaw04NWOKc7AcXrGnuM3MzxAb&ust=1653224649513000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLDRofzT8PcCFQAAAAAdAAAAABAR",
-  },
-  {
-    postsBody:
-      "Once upon a time there lived a lion in a forest. One day after a heavy meal. It was sleeping under a tree. After a while, there came a mouse and it started to play on the lion. Suddenly the lion got up with anger and looked for those who disturbed its nice sleep. Then it saw a small mouse standing trembling with fear. The lion jumped on it and started to kill it. The mouse requested the lion to forgive it. The lion felt pity and left it. The mouse ran away.On another day, the lion was caught in a net by a hunter. The mouse came there and cut the net. Thus it escaped. There after, the mouse and the lion became friends. They lived happily in the forest afterwards.",
-  },
-  { postsTag: '<img src="">' },
-];
+console.log(ElementInArray("Aisha", userNames));
+console.log(ElementInArray("Rayana", userNames));
+document.write(ElementInArray("Shabdan"));
